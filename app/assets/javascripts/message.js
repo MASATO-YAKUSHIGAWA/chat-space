9f50e0e64a46307a23b802afd764e4bfe44d21f5
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-06-29 18:39:41 +0900

    edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,5 +3,14 @@ $(function(){
     e.preventDefault();
     // console.log(this)
     var formData = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
   })
 })
