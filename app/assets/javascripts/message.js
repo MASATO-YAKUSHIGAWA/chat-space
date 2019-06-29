8a645aba7b74780ab3e480df334958aec31e7825
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-06-29 20:37:07 +0900

    edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,29 @@
 $(function(){
+  function buildHTML (message){
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = <div class='message'>
+                <div class='upper-message'>
+                <div class='upper-message__user-name'>
+                ${message.user_name}
+                </div>
+                <div class='upper-message__date'>
+                ${message.date}
+                </div>
+                </div>
+                <div class='lower-message'>
+                <p class='lower-message__content'>
+                <div>
+                  ${content}
+                </div>
+                ${img}
+                </p>
+                </div>
+                </div>
+    return html;            
+  }
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     // console.log(this)
@@ -12,5 +37,18 @@ $(function(){
       processData: false,
       contentType: false
     })
+
+    .done(function(data){
+      var html = buldHTML(data);
+      $('.message').append(html);
+      $('#message_content').val('')
+  
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.submit-btn').prop('disabled', false);
   })
+})
 })
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
         format.json
       end
     else
-      @messages = @group.messages.inclide(:user)
+      @messages = @group.messages.includes(:user)
       flash.now[:alert] = "メッセージを入力してください"
       render :index
     end
